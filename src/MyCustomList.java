1e48ab5e67640528338c3d4de011f0bfe2b290c1
aartikansal <60615341+aartikansal@users.noreply.github.com>
2023-02-20 13:09:15 -0500

    Initial commit

---
+++ src/MyCustomList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class MyCustomList {
+    ArrayList<String> list = new ArrayList<>();
+    public void addElement(String element){
+        list.add(element);
+
+    }
+    public void removeElement(String element){
+        list.remove(element);
+    }
+}
